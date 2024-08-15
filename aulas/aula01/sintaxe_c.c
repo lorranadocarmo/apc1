d2ed670e02b1c20ae08c20dda8d9dd519570f4c8
Lorrana do Carmo <142948787+lorranadocarmo@users.noreply.github.com>
2024-08-15 00:48:04 +0000

    codigos da aula 1

---
+++ aulas/aula01/sintaxe_c.c
@@ -0,0 +1,29 @@
+// ISSO é diferente disso
+//crtl  + ;
+//pre processador
+#include <stdio.h> // inclusão de bibliotecas
+
+int main() { 
+  //função principal
+  //codigo aqui
+  
+  //1 entrada
+  // levantar meus ingredientes
+  //char, init, float
+  // variaveis
+  //leitura
+  int primeiro_numero;
+  int segundo_numero;
+  primeiro_numero = 7;
+  segundo_numero = 6;
+  
+  // 2 processamento
+  // calculos soma...
+  int soma =primeiro_numero + segundo_numero;
+
+  //3 saida
+  //escrita
+
+  printf("A soma de %i com %i eh  %i\n", primeiro_numero, segundo_numero, soma);
+  return 0; // retorno fim do programa;
+}
