0f13223f7e82ffd7bd1162f0f7b1b94d230ebd2a
Lorrana do Carmo <142948787+lorranadocarmo@users.noreply.github.com>
2024-08-22 00:55:51 +0000

    Trabalhando com tipos

---
+++ praticas/pratica02/imprima_boletim.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+  printf("Matricula\t" "A1\t" "A2\t" "Média\t\n");
+  printf("%i\t" "%5.f\t" "%5.f\t" "%5.f\t", 20210010, 8.5, 5.0, 6.4 );
+  
+  return 0;
+  
+}
---
+++ praticas/pratica02/imprima_lorrana.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+printf("Lorrana");
+printf("Lorrana\n");
+  printf("\nLorrana\n");
+  
+  return 0;
+}
+
